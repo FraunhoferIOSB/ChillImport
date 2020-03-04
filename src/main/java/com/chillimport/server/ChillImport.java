4d901be66b8ce4f59ba1e66fc80bfa02db232ced
Restyled.io <commits@restyled.io>
2020-03-04 07:21:28 +0000

    Restyled by clang-format

--- src/main/java/com/chillimport/server/ChillImport.java
+++ src/main/java/com/chillimport/server/ChillImport.java
@@ -2,14 +2,11 @@ package com.chillimport.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ChillImport {
-	
 
-    /**
-     * @param args start up arguments
-     */
-    public static void main(String[] args) {
-    }
+  /**
+   * @param args start up arguments
+   */
+  public static void main(String[] args) {}
 }
