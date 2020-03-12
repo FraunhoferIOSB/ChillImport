2579e65ca19cf10b9282ca88d663a74934c7bdfd
Philipp Hertweck <philipp.hertweck@iosb.fraunhofer.de>
2020-03-12 13:28:22 +0100

    Use logger instead of System.out.

--- src/main/java/com/chillimport/server/ServerApplication.java
+++ src/main/java/com/chillimport/server/ServerApplication.java
@@ -1,11 +1,14 @@
 package com.chillimport.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
 
+  private static final Logger LOGGER              = LoggerFactory.getLogger(ServerApplication.class);
   private static final String DEFAULT_CONFIG_PATH = "/data";
 
   /**
@@ -18,23 +21,19 @@ public class ServerApplication {
     String httpsProxy = System.getenv("HTTPS_PROXY");
 
     if (httpProxy != null) {
-      try {
         String httpHost = ((httpProxy.split(":"))[1]).substring(2);
         String httpPort = (httpProxy.split(":"))[2];
 
-        System.out.println("http proxy: " + httpProxy);
         System.setProperty("http.proxySet", "true");
         System.setProperty("http.proxyHost", httpHost);
         System.setProperty("http.proxyPort", httpPort);
-      } catch (NullPointerException e) {
-        System.out.println("Cannot resolve http proxy");
-      }
+        LOGGER.debug("Using http proxy: " + httpProxy);
     } else {
-      System.out.println("No HTTP proxy defined.");
+      LOGGER.debug("No http proxy specified.");
     }
 
     if (httpsProxy != null) {
-      try {
+
         String httpsHost = ((httpsProxy.split(":"))[1]).substring(2);
         String httpsPort = (httpsProxy.split(":"))[2];
 
@@ -42,16 +41,16 @@ public class ServerApplication {
         System.setProperty("https.proxySet", "true");
         System.setProperty("https.proxyHost", httpsHost);
         System.setProperty("https.proxyPort", httpsPort);
-      } catch (NullPointerException e) {
-        System.out.println("Cannot resolve https proxy");
-      }
+
+        LOGGER.debug("Using https proxy: " + httpsProxy);
+
     } else {
-      System.out.println("No HTTP proxy defined.");
+    LOGGER.debug("No https proxy specified.");
     }
 
     if (confvalue == null) {
       confvalue = DEFAULT_CONFIG_PATH;
-      System.out.println("Config path was not specified. Using default:" +
+      LOGGER.info("Config path was not specified. Using default:" +
                          DEFAULT_CONFIG_PATH);
     }
     FileManager.setPathsOnStartup(confvalue);
@@ -59,7 +58,7 @@ public class ServerApplication {
 
     if (!username.equals("")) {
       FileManager.setUsernameOnStartup(username);
-      System.out.println("Found user name");
+      LOGGER.info("Found user name");
     }
     SpringApplication.run(ServerApplication.class, args);
   }
