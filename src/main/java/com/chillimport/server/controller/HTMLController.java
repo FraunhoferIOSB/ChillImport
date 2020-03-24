872c10bf2a027a7c9bd6c45000d6ee830e4e16b5
Cierpka <adrian.cierpka@iosb.fraunhofer.de>
2020-03-24 14:43:57 +0100

    fix for pingFrostServer

--- src/main/java/com/chillimport/server/controller/HTMLController.java
+++ src/main/java/com/chillimport/server/controller/HTMLController.java
@@ -41,197 +41,194 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class HTMLController {
 
-  private @Autowired HttpServletRequest request;
+	private @Autowired HttpServletRequest request;
 
-  @Autowired private SensorThingsServiceFactory sensorThingsServiceFactory;
+	@Autowired
+	private SensorThingsServiceFactory sensorThingsServiceFactory;
 
-  @RequestMapping(value = "/websitepreview", method = RequestMethod.POST)
-  public ResponseEntity<?> websitePreview(@RequestParam String s)
-      throws MalformedURLException, InvalidFormatException {
-    FileManager fm = new FileManager();
-    File file = fm.storeFromURL(s);
-    // möglicherweise instabil
+	@RequestMapping(value = "/websitepreview", method = RequestMethod.POST)
+	public ResponseEntity<?> websitePreview(@RequestParam String s)
+			throws MalformedURLException, InvalidFormatException {
+		FileManager fm = new FileManager();
+		File file = fm.storeFromURL(s);
+		// möglicherweise instabil
 
-    Configuration sampleConfig;
-    ArrayList<ArrayList<String>> firstThreeRowsOfTable;
+		Configuration sampleConfig;
+		ArrayList<ArrayList<String>> firstThreeRowsOfTable;
 
-    if (file.getName().endsWith(".xls") || file.getName().endsWith(".xlsx")) {
-      sampleConfig = new Configuration(0, ";", DataType.EXCEL);
-    } else if (file.getName().endsWith(".csv")) {
-      sampleConfig = new Configuration(0, ";", DataType.CSV);
-    } else {
-      return new ResponseEntity<>("File is not XSLX,XLS,CSV",
-                                  HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+		if (file.getName().endsWith(".xls") || file.getName().endsWith(".xlsx")) {
+			sampleConfig = new Configuration(0, ";", DataType.EXCEL);
+		} else if (file.getName().endsWith(".csv")) {
+			sampleConfig = new Configuration(0, ";", DataType.CSV);
+		} else {
+			return new ResponseEntity<>("File is not XSLX,XLS,CSV", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-    try {
-      firstThreeRowsOfTable = UploadHandler.preview(file, sampleConfig);
-    } catch (IOException e) {
-      ErrorHandler.getInstance().addRows(-1, e);
-      return new ResponseEntity<>("Failed to convert file",
-                                  HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+		try {
+			firstThreeRowsOfTable = UploadHandler.preview(file, sampleConfig);
+		} catch (IOException e) {
+			ErrorHandler.getInstance().addRows(-1, e);
+			return new ResponseEntity<>("Failed to convert file", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-    return new ResponseEntity<>(firstThreeRowsOfTable, HttpStatus.OK);
-  }
+		return new ResponseEntity<>(firstThreeRowsOfTable, HttpStatus.OK);
+	}
 
-  @RequestMapping(value = "/errors/returnFiles", method = RequestMethod.GET)
-  @ResponseBody
-  public String returnFile() {
-    return ErrorHandler.getInstance().returnFiles();
-  }
+	@RequestMapping(value = "/errors/returnFiles", method = RequestMethod.GET)
+	@ResponseBody
+	public String returnFile() {
+		return ErrorHandler.getInstance().returnFiles();
+	}
 
-  /*
-  @RequestMapping(value = "/server-check", method = RequestMethod.GET)
-  @ResponseBody
-  public boolean pingFROSTServer() {
-      try {
-          return
-  InetAddress.getByName(FileManager.getServerURL().getHost()).isReachable(6000);
-      } catch (UnknownHostException e) {
-          LogManager.getInstance().writeToLog("Server not reachable", true);
-          ErrorHandler.getInstance().addRows(-1,e);
-          return false;
-      } catch (MalformedURLException e) {
-          LogManager.getInstance().writeToLog("Server address malformed", true);
-          ErrorHandler.getInstance().addRows(-1,e);
-          return false;
-      } catch (IOException e) {
-          LogManager.getInstance().writeToLog("Internet connection offline",
-  true); ErrorHandler.getInstance().addRows(-1,e); return false;
-      }
-  }
-  */
+	/*
+	 * @RequestMapping(value = "/server-check", method = RequestMethod.GET)
+	 * 
+	 * @ResponseBody public boolean pingFROSTServer() { try { return
+	 * InetAddress.getByName(FileManager.getServerURL().getHost()).isReachable(6000)
+	 * ; } catch (UnknownHostException e) {
+	 * LogManager.getInstance().writeToLog("Server not reachable", true);
+	 * ErrorHandler.getInstance().addRows(-1,e); return false; } catch
+	 * (MalformedURLException e) {
+	 * LogManager.getInstance().writeToLog("Server address malformed", true);
+	 * ErrorHandler.getInstance().addRows(-1,e); return false; } catch (IOException
+	 * e) { LogManager.getInstance().writeToLog("Internet connection offline",
+	 * true); ErrorHandler.getInstance().addRows(-1,e); return false; } }
+	 */
 
-  @RequestMapping(value = "/server-check", method = RequestMethod.GET)
-  @ResponseBody
-  public String pingFROSTServer(@RequestParam String frostUrl) {
+	@RequestMapping(value = "/server-check", method = RequestMethod.GET)
+	@ResponseBody
+	public String pingFROSTServer(@RequestParam String frostUrl) {
 
-    SensorThingsService service;
+		SensorThingsService service;
 
-    try {
-      URL url = new URL(frostUrl);
-      InetAddress.getByName(url.getHost()).isReachable(6000);
+		try {
+			URL url = new URL(frostUrl);
+			InetAddress.getByName(url.getHost()).isReachable(6000);
 
-      service = sensorThingsServiceFactory.build(new URL(frostUrl));
+			service = sensorThingsServiceFactory.build(new URL(frostUrl));
 
-      service.things().query().first();
-    } catch (UnknownHostException e) {
-      LogManager.getInstance().writeToLog("Server not reachable", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return "Server not reachable";
-    } catch (MalformedURLException e) {
-      LogManager.getInstance().writeToLog("Server address malformed", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return "Server address malformed";
-    } catch (IOException e) {
-      LogManager.getInstance().writeToLog("Internet connection offline", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return "Internet connection offline";
-    } catch (URISyntaxException e) {
-      LogManager.getInstance().writeToLog("Wrong URI for Frost-Server", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return "Wrong URI for Frost-Server";
-    } catch (ServiceFailureException e) {
-      LogManager.getInstance().writeToLog("Server not reachable", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return "Server not reachable";
-    }
+			service.things().query().first();
+		} catch (UnknownHostException e) {
+			LogManager.getInstance().writeToLog("Server not reachable", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return "Server not reachable";
+		} catch (MalformedURLException e) {
+			LogManager.getInstance().writeToLog("Server address malformed", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return "Server address malformed";
+		} catch (IOException e) {
+			LogManager.getInstance().writeToLog("Internet connection offline", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return "Internet connection offline";
+		} catch (URISyntaxException e) {
+			LogManager.getInstance().writeToLog("Wrong URI for Frost-Server", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return "Wrong URI for Frost-Server";
+		} catch (ServiceFailureException e) {
+			LogManager.getInstance().writeToLog("Server not reachable", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return "Server not reachable";
+		}
 
-    return "Server reachable";
-  }
+		return "Server reachable";
+	}
 
-  public boolean pingFROSTServer(URL frostUrl) {
+	public boolean pingFROSTServer(URL frostUrl) {
+		SensorThingsServiceFactory sFactory = new SensorThingsServiceFactory();
+		SensorThingsService service;
 
-    try {
-      return InetAddress.getByName(frostUrl.getHost()).isReachable(6000);
-    } catch (UnknownHostException e) {
-      LogManager.getInstance().writeToLog("Server not reachable", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return false;
-    } catch (MalformedURLException e) {
-      LogManager.getInstance().writeToLog("Server address malformed", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return false;
-    } catch (IOException e) {
-      LogManager.getInstance().writeToLog("Internet connection offline", true);
-      ErrorHandler.getInstance().addRows(-1, e);
-      return false;
-    }
-  }
+		try {
+			InetAddress.getByName(frostUrl.getHost()).isReachable(6000);
+			service = sFactory.build(frostUrl);
+			service.things().query().first();
+			return true;
+		} catch (UnknownHostException e) {
+			LogManager.getInstance().writeToLog("Server not reachable", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return false;
+		} catch (MalformedURLException e) {
+			LogManager.getInstance().writeToLog("Server address malformed", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return false;
+		} catch (IOException e) {
+			LogManager.getInstance().writeToLog("Internet connection offline", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return false;
+		} catch (URISyntaxException e) {
+			LogManager.getInstance().writeToLog("Wrong URI for Frost-Server", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return false;
+		} catch (ServiceFailureException e) {
+			LogManager.getInstance().writeToLog("Server not reachable", true);
+			ErrorHandler.getInstance().addRows(-1, e);
+			return false;
+		}
+	}
 
-  @RequestMapping(value = "errors/delFile", method = RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity delFile(@RequestParam String name) {
-    File file = new File(FileManager.getLogPath() + File.separator +
-                         "returnRows" + File.separator + name);
-    if (!file.delete()) {
-      return new ResponseEntity("Could not delete File",
-                                HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-    return new ResponseEntity("Deleted File", HttpStatus.OK);
-  }
-  @GetMapping("/returnRows/{fileName:.+}")
-  public ResponseEntity<?> downloadFile(@PathVariable String fileName,
-                                        HttpServletRequest request) {
-    FileManager fm = new FileManager();
-    Resource res = fm.download(fileName);
-    String contentType;
-    try {
-      contentType = request.getServletContext().getMimeType(
-          res.getFile().getAbsolutePath());
-    } catch (IOException e) {
-      contentType = "application/octet-stream";
-    }
-    if (contentType == null) {
-      contentType = "application/octet-stream";
-    }
+	@RequestMapping(value = "errors/delFile", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity delFile(@RequestParam String name) {
+		File file = new File(FileManager.getLogPath() + File.separator + "returnRows" + File.separator + name);
+		if (!file.delete()) {
+			return new ResponseEntity("Could not delete File", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity("Deleted File", HttpStatus.OK);
+	}
 
-    return ResponseEntity.ok()
-        .contentType(MediaType.parseMediaType(contentType))
-        .header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + res.getFilename() + "\"")
-        .body(res);
-  }
+	@GetMapping("/returnRows/{fileName:.+}")
+	public ResponseEntity<?> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+		FileManager fm = new FileManager();
+		Resource res = fm.download(fileName);
+		String contentType;
+		try {
+			contentType = request.getServletContext().getMimeType(res.getFile().getAbsolutePath());
+		} catch (IOException e) {
+			contentType = "application/octet-stream";
+		}
+		if (contentType == null) {
+			contentType = "application/octet-stream";
+		}
 
-  @GetMapping("/get-return")
-  public ResponseEntity<?> downloadFile(@RequestParam String fileName) {
-    FileManager fm = new FileManager();
-    Resource res = fm.download(fileName);
-    String contentType;
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + res.getFilename() + "\"")
+				.body(res);
+	}
 
-    try {
-      contentType = this.request.getServletContext().getMimeType(
-          res.getFile().getAbsolutePath());
-    } catch (IOException e) {
-      contentType = "application/octet-stream";
-    }
+	@GetMapping("/get-return")
+	public ResponseEntity<?> downloadFile(@RequestParam String fileName) {
+		FileManager fm = new FileManager();
+		Resource res = fm.download(fileName);
+		String contentType;
 
-    if (contentType == null) {
-      contentType = "application/octet-stream";
-    }
+		try {
+			contentType = this.request.getServletContext().getMimeType(res.getFile().getAbsolutePath());
+		} catch (IOException e) {
+			contentType = "application/octet-stream";
+		}
 
-    return ResponseEntity.ok()
-        .contentType(MediaType.parseMediaType(contentType))
-        .header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + res.getFilename() + "\"")
-        .body(res);
-  }
+		if (contentType == null) {
+			contentType = "application/octet-stream";
+		}
 
-  // noch benötigt?
-  @RequestMapping(value = "/getfrosturl", method = RequestMethod.GET)
-  @ResponseBody
-  public String getFrostServerURL(@RequestParam String urlasString) {
-    try {
-      URL url = new URL(urlasString);
-      InetAddress.getByName(url.getHost()).isReachable(6000);
-      return url.toString();
-    } catch (UnknownHostException e) {
-      return "Server not reachable";
-    } catch (MalformedURLException e) {
-      return "FROST URL malformed";
-    } catch (IOException e) {
-      return "Internet connection offline";
-    }
-  }
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + res.getFilename() + "\"")
+				.body(res);
+	}
+
+	// noch benötigt?
+	@RequestMapping(value = "/getfrosturl", method = RequestMethod.GET)
+	@ResponseBody
+	public String getFrostServerURL(@RequestParam String urlasString) {
+		try {
+			URL url = new URL(urlasString);
+			InetAddress.getByName(url.getHost()).isReachable(6000);
+			return url.toString();
+		} catch (UnknownHostException e) {
+			return "Server not reachable";
+		} catch (MalformedURLException e) {
+			return "FROST URL malformed";
+		} catch (IOException e) {
+			return "Internet connection offline";
+		}
+	}
 }
